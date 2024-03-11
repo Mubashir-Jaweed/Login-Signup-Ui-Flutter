0d2a2f6b61a589338ba19388c3dd78c4d6e5f670
MubashirJ <mubashirmjjawed@gmail.com>
2024-03-11 12:06:25 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:myapp/Activity/home.dart';
+import 'package:myapp/Activity/loading.dart';
+import 'package:myapp/Activity/location.dart';
+void main() {
+  runApp(const MaterialApp(
+    home:Home(),
+  ));
+}
